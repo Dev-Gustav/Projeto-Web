f457a888352b43b899a341a9a0f2cab37dc78f28
Luis Gustavo Fernandes Cardoso <162601657+Golias-dev@users.noreply.github.com>
2025-01-19 03:37:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="Author = " content="Golias-Dev">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- END GOOGLE FONTS -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <title>Gustavo Dev</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+					<img src="./img/foto logo 2.jpeg" alt="foto de perfil" oncontextmenu="return false;" />
+            <h1>@Luisfernnandes__</h1>
+            <p>Luis Gustavo Fernandes Cardoso | UX e UI Designer | Dev<br></p>
+        </header>
+
+        <section class="botoes-links">
+
+							
+            <a href="https://www.linkedin.com/in/luis-gustavo-fernandes-cardoso-726bb12a7/" target="_blank" class="link">
+                <button class="degrade">
+                    <span>LinkedIn</span>
+                    <i class="bi bi-linkedin"></i> 
+                </button>
+            </a>
+            <a href="https://www.instagram.com/Luisfernnandes__/" target="_blank" class="link">
+                <button class="degrade">
+                    <span>Instagram</span>
+                    <i class="bi bi-instagram"></i>
+                </button >
+            </a>
+            <a href="https://github.com/Golias-dev" target="_blank" class="link">
+                <button class="degrade">
+                    <span>Github</span>
+                    <i class="bi bi-github"></i>
+                </button >
+            </a>
+
+            <a href="https://vsco.co/lgustfer/gallery" target="_blank" class="link">
+                <button class="degrade">
+                    <span>VSCO</span>
+                    <i class="bi bi-camera"></i>
+                </button >
+            </a>
+
+						<div class= "icons-button">
+								<a href="https://www.linkedin.com/in/luis-gustavo-fernandes-cardoso-726bb12a7/" class="btn-footer " target="_blank"><i class="bi bi-linkedin"></i></a>
+								<a href="https://www.instagram.com/Luisfernnandes__/" class="btn-footer " target="_blank"><i class="bi bi-instagram"></i></a>
+								<a href="https://github.com/Golias-dev" class="btn-footer " target="_blank"><i class="bi bi-github"></i></a>
+								<a href="https://vsco.co/lgustfer/gallery" class="btn-footer " target="_blank"><i class="bi bi-camera"></i></a>
+				</div><!--container-->
+        </section><!--botoes-links-->
+  <!-- Footer -->
+  <footer>
+    <p>Desenvolvido por <strong>Gustavo dev&copy;</strong></p>
+  </footer>
+			</div>
+				
+</body>
+</html>
